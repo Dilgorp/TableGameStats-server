115b8e521decb57df206b8031b9591af86a110ec
Konstantin Cherepanov <koncherepanov@gmail.com>
2020-11-07 11:56:19 +0500

    Протестирована работа с базой

---
+++ src/main/java/ru/dilgorp/java/stats/game/table/response/ResponseType.java
@@ -0,0 +1,10 @@
+package ru.dilgorp.java.stats.game.table.response;
+
+/**
+ * Перечисление для типов ответов
+ * SUCCESS - запрос выполнен успешно
+ * ERROR - запрос выполнен с ошибкой
+ */
+public enum ResponseType {
+    SUCCESS, ERROR
+}
