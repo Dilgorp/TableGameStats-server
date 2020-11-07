ccc5a3d3671d3bf5b4901d0c5a4770696f5e8e57
Konstantin Cherepanov <koncherepanov@gmail.com>
2020-11-07 09:30:40 +0500

    Initial commit

---
+++ src/main/java/ru/dilgorp/java/stats/game/table/TableGameStatsApplication.java
@@ -0,0 +1,13 @@
+package ru.dilgorp.java.stats.game.table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableGameStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableGameStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dilgorp/java/stats/game/table/TableGameStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dilgorp.java.stats.game.table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableGameStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
